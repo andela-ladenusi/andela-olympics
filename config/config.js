01ad9f6b1c837d8fc4e51f713e1e757da8e0a285
Olatunde Garuba <garuba.olatunde@andela.co>
2015-05-22 00:17:50 +0100

    root base changed

--- config/config.js
+++ config/config.js
@@ -1,27 +1,27 @@
 var development = {
   firebase: {
-    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    rootRefUrl: 'https://andela-olympics-dev.firebaseio.com/',
     secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
   }
 };
 
 var test = {
   firebase: {
-    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    rootRefUrl: 'https://andela-olympics-dev.firebaseio.com/',
     secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
   }
 };
 
 var production = {
   firebase: {
-    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    rootRefUrl: 'https://andela-olympics-dev.firebaseio.com/',
     secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
   }
 };
 
 var staging = {
   firebase: {
-    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    rootRefUrl: 'https://andela-olympics-dev.firebaseio.com/',
     secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
   }
 };

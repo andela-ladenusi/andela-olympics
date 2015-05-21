84cf02a9951093480968d86fdff097f05635488d
Ayoola Solomon <ayoola.solomon@andela.co>
2015-05-21 18:32:28 +0100

    Initial Commit

---
+++ api/routes/index.js
@@ -0,0 +1,9 @@
+var users = require('./users.routes');
+var questions = require('./questions.routes');
+var answers = require('./answers.routes.js');
+
+module.exports = function(app, config) {
+  users(app, config);
+  questions(app, config);
+  answers(app, config);
+};
---
+++ config/config.js
@@ -0,0 +1,36 @@
+var development = {
+  firebase: {
+    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
+  }
+};
+
+var test = {
+  firebase: {
+    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
+  }
+};
+
+var production = {
+  firebase: {
+    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
+  }
+};
+
+var staging = {
+  firebase: {
+    rootRefUrl: 'https://knowledgebot.firebaseio.com/',
+    secretKey: 'eMk9rNB5UWkc2E1bw5QPItx4e5JznCZIXzl1deRF'
+  }
+};
+
+var config = {
+  development: development,
+  test: test,
+  production: production,
+  staging: staging
+};
+
+module.exports = config;

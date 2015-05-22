181bd61397cebe7834907e88d78082522248a978
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-05-22 21:06:06 +0100

    started structuring

--- app/application.js
+++ app/application.js
@@ -1,3 +1,26 @@
+// defining modules
+angular.module('olympics.controllers', []);
+angular.module('olympics.services', []);
+
+/* loading services */
+require('./js/services/auth.js');
+require('./js/services/refs.js');
+
+window.Olympics = angular.module('Olympics', [
+	'ui.router',
+	'olympics.controllers',
+	'olympics.services'
+]);
+
+
+
+
+
+
+
+
+
+
 $(function() {
 	$('.how-link').on('click', function() {
 		console.log("Clicked");
---
+++ app/js/services/refs.js
@@ -0,0 +1,13 @@
+angular.module('olympics.services')
+  .factory('Refs', ['$cookies', '$firebase',
+    function($cookies, $firebase) {
+      var rootRef = new Firebase($cookies.rootRef || 'YOUR_FIREBASE_URL');     
+      
+      // define every standard ref used application wide
+      return {
+        root: rootRef,
+        users: rootRef.child('users'),
+        competitions: rootRef.child('competitions')
+      };
+    }
+  ]);
